23fa0dea9582dec88571484bceddbb695d86e5a3
girmachew82 <girmachewabebe@gmail.com>
2025-02-01 09:38:49 +0300

    Getter and Setter

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -29,8 +29,10 @@ class ProductController extends Controller
     //$product = ProductController::$products[$id-1];
     $product = Product::findOrFail($id);
 
-    $viewData["title"] = $product["name"]." - Online Store";
-    $viewData["subtitle"] = $product["name"]." - Product information";
+    // $viewData["title"] = $product["name"]." - Online Store";
+    // $viewData["subtitle"] = $product["name"]." - Product information";
+    $viewData["title"] = $product->getName()." - Online Store";
+    $viewData["subtitle"] = $product->getName()." - Product information";
     $viewData["product"] = $product;
     return view('product.show')->with("viewData", $viewData);
     }
